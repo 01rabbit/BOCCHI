d3fd9175f7a564dbc225b946c1f92637a6d81e51
Mr.Rabbit <appleseedj072@gmail.com>
2023-10-09 22:58:27 +0900

    bot create

--- app.py
+++ app.py
@@ -1,25 +1,47 @@
 import json
-from flask import Flask, render_template, request
-import json
-import slackweb
+from flask import Flask, request
+import requests
+
+BOT_TOKEN = 'i3kjhoc6k3fzuqsdejquhr5bwr'
+CHANNEL_ID = '5zeseysurpbudp114wgs7zsosa' #Town Square
+MM_API_ADDRESS = 'http://127.0.0.1/api/v4/posts' 
 
 app = Flask(__name__)
-mattermost = slackweb.Slack(url="http://127.0.0.1/hooks/bbw4mq7jhbgaumba3mg6ce1grw")
 
 @app.route("/")
 def hello():
     return "HelloWorld"
 
 @app.route("/matter", methods=['POST'])
-def post():
-    date = request.json
-    text = date['text']
-    _, newtext = text.split(' ')
+def bot_reply():
+    posted_user = request.json['user_name']
+    posted_msg = request.json['text']
 
-    text = ''.join(newtext)
-    mattermost.notify(text=text)
-    print(date)
-    return json.dumps(dict())
+    reply_headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + BOT_TOKEN,
+    }
+
+    reply_data = {
+        "channel_id": CHANNEL_ID,
+        "message": f"@{posted_user} Bot reply message.",
+        "props": {
+            "attachments": [
+                    {
+                "author_name": posted_user,
+                "text": posted_msg,
+                }
+            ]
+        },
+    }
+
+    reply_request = requests.post(
+        MM_API_ADDRESS,
+        headers = reply_headers,
+        data = json.dumps(reply_data)
+    )
+
+    return reply_request
 
 if __name__ == '__main__':
     app.debug = True

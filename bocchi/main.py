bba6512622e54c049da7187323555f916300155e
Mr.Rabbit <appleseedj073@gmail.com>
2023-11-21 19:20:06 +0900

    実行の有無を問う様に変更

--- bocchi/main.py
+++ bocchi/main.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python3
 
+import datetime
 import os
+import random
+import subprocess
+import threading
+from flask import Flask, request, jsonify
 from janome.analyzer import Analyzer
 from janome.tokenfilter import *
-import random
-from flask import Flask, request
-import subprocess
-import datetime
 from socket import inet_aton
-from bocchi.matterCommunicator import messenger
+from bocchi.matterCommunicator import *
 from bocchi.config import brutespray_conf as b_conf
 import bocchi.faradayController as fc
 import bocchi.gvmController as gc
 
 app = Flask(__name__)
 
-# commands = ['表示', 'スキャン', '脆弱性診断','認証試行']
-
+# ---------------------------------------------------------------------
+# valid_ip
+# ---------------------------------------------------------------------
 def valid_ip(order):
     for word in order:
         try:
@@ -25,9 +27,15 @@ def valid_ip(order):
         except:
             None
 
+# ---------------------------------------------------------------------
+# get_timestamp
+# ---------------------------------------------------------------------
 def get_timestamp():
     return datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
 
+# ---------------------------------------------------------------------
+# get_menu
+# ---------------------------------------------------------------------
 def get_menu():
     menuText = "## メニュー\n\
 ### 調査系\n\
@@ -60,6 +68,9 @@ def get_menu():
 :warning: WakeUp Code（**@bocchi**,**@ぼっち**）の後、半角スペースを入れてから要件をお伝えください。"
     return menuText
 
+# ---------------------------------------------------------------------
+# check_dir
+# ---------------------------------------------------------------------
 def check_dir(targetIP):
     dirPath = f"results/{targetIP}"
     if os.path.isdir(dirPath):
@@ -68,6 +79,9 @@ def check_dir(targetIP):
         os.mkdir(dirPath)
         return dirPath
 
+# ---------------------------------------------------------------------
+# answer
+# ---------------------------------------------------------------------
 def answer():
     rand = random.randint(1,20)
     if rand % 3 == 0 and rand % 5 == 0:
@@ -80,11 +94,154 @@ def answer():
         messages = "呼びました？"
     return messages
 
+# ---------------------------------------------------------------------
+# perform_full_port_scan
+# ---------------------------------------------------------------------
+def perform_full_port_scan(ipaddr, posted_user):
+    """
+    フルポートスキャンを実行し、結果をMattermostおよびFaradayに通知するメソッド。
+
+    Parameters:
+        ipaddr (str): スキャン対象のIPアドレス
+        posted_user (str): Mattermostへの通知時に使用するユーザー情報
+
+    Returns:
+        str: スキャン結果およびFaradayのインポート結果のメッセージ
+    """
+    # フルポートスキャンの開始連絡
+    messages = f"nmapで{ipaddr}のフルポートスキャンを開始します。\nしばらくお待ちください:coffee:"
+    send_message_to_user(posted_user=posted_user, msg=messages)
+
+    # nmapをsubprocessで呼び出してフルポートスキャンを実行
+    resultsPath = check_dir(targetIP=ipaddr)
+    subprocess.run(["nmap", "-vv", "--reason", "-Pn", "-T4", "-sV", "-sC", "--version-all", "-A", "-p-", "--osscan-guess", "--script=vuln", "-oA", f"{resultsPath}/FullScan_{get_timestamp()}", ipaddr])
+    messages = f"{ipaddr}のフルポートスキャンが終了しました。"
+
+    # Faradayへの結果のインポート
+    addText = fc.import_from_results(dir_path=resultsPath)
+
+    # 終了連絡の追加
+    messages += f"\n{addText}"
+
+    send_message_to_user(posted_user, messages)
+
+# ---------------------------------------------------------------------
+# perform_standard_scan
+# ---------------------------------------------------------------------
+def perform_standard_scan(ipaddr, posted_user):
+    """
+    標準スキャンを実行し、結果をMattermostおよびFaradayに通知するメソッド。
+
+    Parameters:
+        ipaddr (str): スキャン対象のIPアドレス
+        posted_user (str): Mattermostへの通知時に使用するユーザー情報
+
+    Returns:
+        str: スキャン結果およびFaradayのインポート結果のメッセージ
+    """
+    
+    # 標準スキャンの開始連絡
+    messages = f"nmapで{ipaddr}のスキャンを開始します。\nしばらくお待ちください:coffee:"
+    send_message_to_user(posted_user=posted_user, msg=messages)
+
+    # nmapをsubprocessで呼び出してスキャンを実行
+    resultsPath = check_dir(targetIP=ipaddr)
+    subprocess.run(["nmap", "-vv", "--reason", "-Pn", "-T4", "-sV", "-sC", "--version-all", "-A", "--osscan-guess", "--script=vuln", "-oA", f"{resultsPath}/Scan_{get_timestamp()}", ipaddr])
+    messages = f"{ipaddr}のスキャンが終了しました。"
+
+    # Faradayへの結果のインポート
+    addText = fc.import_from_results(dir_path=resultsPath)
+
+    # 終了連絡の追加
+    messages += f"\n{addText}"
+
+    send_message_to_user(posted_user, messages)
+
+# ---------------------------------------------------------------------
+# perform_vulnerability_scan_with_gvm
+# ---------------------------------------------------------------------
+def perform_vulnerability_scan_with_gvm(ipaddr, posted_user):
+    """
+    GVM(Openvas)を使用して脆弱性診断を実行し、結果をMattermostに通知するメソッド。
+
+    Parameters:
+        ipaddr (str): スキャン対象のIPアドレス
+        posted_user (str): Mattermostへの通知時に使用するユーザー情報
+
+    Returns:
+        str: 脆弱性スキャン結果のメッセージ
+    """
+    # 脆弱性スキャンの開始連絡
+    messages = f"GVM(Openvas)で{ipaddr}の脆弱性診断を開始します。\nしばらくお待ちください:coffee:"
+    send_message_to_user(posted_user=posted_user, msg=messages)
+
+    # ターゲットの作成
+    targetID = gc.getTargetID(ipaddr)
+
+    # タスクの作成
+    taskID = gc.getTaskID(targetID)
+
+    # タスクの開始
+    gc.startTask(taskID)
+
+    # タスクの終了待機
+    gc.checkStatus(taskID)
+
+    # GVM結果の取得
+    messages = gc.check_gvm(ipaddr)
+
+    send_message_to_user(posted_user, messages)
+
+# ---------------------------------------------------------------------
+# perform_brutespray_attack
+# ---------------------------------------------------------------------
+def perform_brutespray_attack(ipaddr, posted_user):
+    """
+    Brutesprayを使用して認証攻撃を実行し、結果をMattermostに通知するメソッド。
+
+    Parameters:
+        ipaddr (str): 攻撃対象のIPアドレス
+        posted_user (str): Mattermostへの通知時に使用するユーザー情報
+
+    Returns:
+        str: 認証攻撃の結果メッセージ
+    """
+    # Brutespray攻撃のパラメータ取得
+    params = b_conf()
+    USER = params['user_list']
+    PASS = params['password_list']
+
+    # 攻撃結果保存ディレクトリの作成
+    resultsPath = check_dir(targetIP=ipaddr)
+
+    # 認証攻撃の開始連絡
+    messages = "認証試行を開始します。\nしばらくお待ちください:coffee:"
+    send_message_to_user(posted_user, msg=messages)
+
+    # Nmapを使用してサービスバージョンを取得
+    subprocess.run(["nmap", "-vv", "-Pn", "-T4", "-sV", "-oG", f"{resultsPath}/result", ipaddr])
+
+    # Brutesprayで認証攻撃を実行
+    subprocess.run(["brutespray", "--file", f"{resultsPath}/result", "-U", f"{USER}", "-P", f"{PASS}", "-o", f"{resultsPath}", "--threads", "5"])
+
+    # 結果ファイルから結果を取得
+    dir_list = os.listdir(resultsPath)
+    results = ""
+    for i in range(len(dir_list)):
+        if ".txt" == os.path.splitext(dir_list[i])[1]:
+            filePath = os.path.join(resultsPath, dir_list[i])
+            with open(filePath) as f:
+                results += f.read()
+
+    # 認証攻撃終了連絡
+    messages = "認証試行終了。\n" + results
+
+    send_message_to_user(posted_user, messages)
+
 @app.route("/matter", methods=['POST'])
 def bot_reply():
     posted_user = request.json['user_name']
     posted_msg = request.json['text']
-    messages=""
     try:
         # トリガーワードと命令文の分解
         firstSegment = posted_msg.split(" ")
@@ -107,107 +264,132 @@ def bot_reply():
                 # nmapを使用した各種スキャン
                 if "フルポートスキャン" in userOrder:
                     # フルポートスキャン
-                    # Mattermostへの開始連絡
-                    messages = f"nmapで{ipaddr}のフルポートスキャンを開始します。\nしばらくお待ちください:coffee:"
-                    messenger(posted_user=posted_user, msg=messages)
-
-                    # nmapをsubprocessで呼び出してフルポートスキャンを実行
-                    resultsPath = check_dir(targetIP=ipaddr)
-                    subprocess.run(["nmap","-vv","--reason","-Pn","-T4","-sV","-sC","--version-all","-A","-p-","--osscan-guess","--script=vuln","-oA",f"{resultsPath}/FullScan_"+get_timestamp(),ipaddr])
-                    messages = f"{ipaddr}のフルポートスキャンが終了しました。"
-
-                    # faradayへ結果のインポート
-                    addText = fc.import_from_results(dir_path=resultsPath)
-
-                    # Mattermostへの終了連絡
-                    messages += f"\n{addText}"
-                    # 標準スキャン
+                    askForFullScanConfirmation(posted_user, ipaddr)
                 elif "スキャン" in userOrder:
-                    # Mattermostへの開始連絡
-                    messages = f"nmapで{ipaddr}のスキャンを開始します。\nしばらくお待ちください:coffee:"
-                    messenger(posted_user=posted_user, msg=messages)
-
-                    # nmapをsubprocessで呼び出してスキャンを実行
-                    resultsPath = check_dir(targetIP=ipaddr)
-                    subprocess.run(["nmap","-vv","--reason","-Pn","-T4","-sV","-sC","--version-all","-A","--osscan-guess","--script=vuln","-oA",f"{resultsPath}/Scan_"+get_timestamp(),ipaddr])
-                    messages = f"{ipaddr}のスキャンが終了しました。"
-
-                    # faradayへ結果のインポート
-                    addText = fc.import_from_results(dir_path=resultsPath)
-
-                    # Mattermostへの終了連絡
-                    messages += f"\n{addText}"
-                # GVMを使用した脆弱性診断
+                    # 標準スキャン
+                    askForStdScanConfirmation(posted_user, ipaddr)
+                    return
                 elif "脆弱性診断" in userOrder:
-                    # Mattermostへの開始連絡
-                    messages = f"GVM(Openvas)で{ipaddr}の脆弱性診断を開始します。\nしばらくお待ちください:coffee:"
-                    messenger(posted_user=posted_user, msg=messages)
-
-                    # ターゲットの作成
-                    targetID = gc.getTargetID(ipaddr)
-
-                    # タスクの作成
-                    taskID = gc.getTaskID(targetID)
-
-                    # タスクの開始
-                    gc.startTask(taskID)
-
-                    # タスクの終了待機
-                    gc.checkStatus(taskID)
-                    messages = gc.check_gvm(ipaddr)
-                    # IPアドレスがないと診断できない
-                # Brutesprayで認証攻撃
+                    # GVMを使用した脆弱性診断
+                    askForVulnScanConfirmation(posted_user, ipaddr)
                 elif "認証試行" in userOrder:
-                    params = b_conf()
-                    USER = params['user_list']
-                    PASS = params['password_list']
-
-                    resultsPath = check_dir(targetIP=ipaddr)
-                    messages = "認証試行を開始します。\nしばらくお待ちください:coffee:"
-                    messenger(posted_user=posted_user, msg=messages)
-                    subprocess.run(["nmap","-vv","-Pn","-T4","-sV","-oG",f"{resultsPath}/result",ipaddr])
-                    subprocess.run(["brutespray", "--file", f"{resultsPath}/result", "-U", f"{USER}", "-P", f"{PASS}", "-o", f"{resultsPath}", "--threads", "5"])
-
-                    dir_list = os.listdir(resultsPath)
-                    results = ""
-
-                    for i in range(len(dir_list)):
-
-                        if ".txt" == os.path.splitext(dir_list[i])[1]: ## os.path.splitext()[1]で拡張子を取得
-                            filePath = os.path.join(resultsPath, dir_list[i])
-                            print(filePath)
-                            with open(filePath) as f:
-                                results += f.read()
-                    messages = "認証試行終了。\n" + results
+                    # Brutesprayで認証攻撃
+                    askForBruteAttackConfirmation(posted_user, ipaddr)
             else:
-                messages = ":no_entry: 指定されたIPアドレスは診断対象外です。\n対象に加えるならば***target_IPs.txt***に追記してください。"
+                send_message_to_user(posted_user,":no_entry: 指定されたIPアドレスは診断対象外です。\n対象に加えるならば***target_IPs.txt***に追記してください。")
                 return
-        elif "表示" in userOrder:
+        elif "表示" in userOrder or "見る" in userOrder or "見せる" in userOrder:
             if "メニュー" in userOrder:
                 # メニューを表示
-                messages = get_menu()
+                send_message_to_user(posted_user,get_menu())
             elif "サービス" in userOrder:
                 # サービスをリストで表示
-                messages = fc.show_service_list()
-            elif "脆弱性診断" in userOrder:
+                send_message_to_user(posted_user,fc.show_service_list())
+            elif "脆弱性診断" in userOrder or "gvm" in userOrder or "GVM" in userOrder:
                 # GVMのダッシュボードを表示
-                messages = gc.show_gvm()
-            elif "脆弱" in userOrder:
+                send_message_to_user(posted_user,gc.show_gvm())
+            elif "脆弱性" in userOrder:
                 # 脆弱性情報をリストで表示
-                messages = fc.show_vuln_list()
+                send_message_to_user(posted_user,fc.show_vuln_list())
             elif "情報" in userOrder or "ファラデー" in userOrder or "faraday" in userOrder or "Faraday" in userOrder:
                 # Faradayのダッシュボードを表示
-                messages = fc.show_faraday()
+                send_message_to_user(posted_user,fc.show_faraday())
         else:
-            messages  = get_menu()
+            send_message_to_user(posted_user,get_menu())
 
     except IndexError:
-        messages = answer()
-
-    finally:
-        messenger(posted_user=posted_user, msg=messages)
+        send_message_to_user(posted_user,answer())
 
     return
+
+@app.route('/actions/std_scan', methods=['POST'])
+def std_scan():
+    data = request.get_json()
+    text = data.get("context", {}).get("text", "")
+    if not text:
+        return jsonify({"ephemeral_text": "Invalid request. Context['text'] is not found."})
+    posted_usr,ipaddr = text.split(",")
+    thread = threading.Thread(target=perform_standard_scan, args=(ipaddr, posted_usr))
+    thread.start()
+
+    response = {
+        "update": {
+            "props": {}
+        },
+        "ephemeral_text": f"{posted_usr}によりスキャンが承認されました。"
+    }
+    
+    return jsonify(response)
+
+@app.route('/actions/full_scan', methods=['POST'])
+def full_scan():
+    data = request.get_json()
+    text = data.get("context", {}).get("text", "")
+    if not text:
+        return jsonify({"ephemeral_text": "Invalid request. Context['text'] is not found."})
+    posted_usr,ipaddr = text.split(",")
+    thread = threading.Thread(target=perform_full_port_scan, args=(ipaddr, posted_usr))
+    thread.start()
+
+    response = {
+        "update": {
+            "props": {}
+        },
+        "ephemeral_text": f"{posted_usr}によりフルポートスキャンが承認されました。"
+    }
+    
+    return jsonify(response)
+
+@app.route('/actions/vuln_scan', methods=['POST'])
+def vuln_scan():
+    data = request.get_json()
+    text = data.get("context", {}).get("text", "")
+    if not text:
+        return jsonify({"ephemeral_text": "Invalid request. Context['text'] is not found."})
+    posted_usr,ipaddr = text.split(",")
+    thread = threading.Thread(target=perform_vulnerability_scan_with_gvm, args=(ipaddr, posted_usr))
+    thread.start()
+
+    response = {
+        "update": {
+            "props": {}
+        },
+        "ephemeral_text": f"{posted_usr}により脆弱性診断が承認されました。"
+    }
+    
+    return jsonify(response)
+
+@app.route('/actions/brute_attack', methods=['POST'])
+def brute_attack():
+    data = request.get_json()
+    text = data.get("context", {}).get("text", "")
+    if not text:
+        return jsonify({"ephemeral_text": "Invalid request. Context['text'] is not found."})
+    posted_usr,ipaddr = text.split(",")
+    thread = threading.Thread(target=perform_brutespray_attack, args=(ipaddr, posted_usr))
+    thread.start()
+
+    response = {
+        "update": {
+            "props": {}
+        },
+        "ephemeral_text": f"{posted_usr}により認証試行が承認されました。"
+    }
+    
+    return jsonify(response)
+
+
+
+@app.route('/actions/reject', methods=['POST'])
+def reject():
+    response = {
+        "update": {
+            "props": {}
+        },
+        "ephemeral_text": "拒否されました。"
+    }
+
+    return jsonify(response)
 
 def main():
     app.debug = True
--- bocchi/matterCommunicator.py
+++ bocchi/matterCommunicator.py
@@ -7,7 +7,20 @@ BOT_TOKEN = params['bot_token']
 CHANNEL_ID = params['channel_id'] #Town Square
 MM_API_ADDRESS = params['mm_api_address']
 
-def messenger(posted_user, msg):
+# ---------------------------------------------------------------------
+# send_message_to_user
+# ---------------------------------------------------------------------
+def send_message_to_user(posted_user, message):
+    """
+    指定されたユーザーにメッセージを送信するメソッド。
+
+    Parameters:
+        posted_user (str): メッセージを受信するユーザーのID
+        message (str): 送信するメッセージの内容
+
+    Returns:
+        requests.Response: Mattermostへのリクエストのレスポンス
+    """
     reply_headers = {
     'Content-Type': 'application/json',
     'Authorization': 'Bearer ' + BOT_TOKEN,
@@ -19,8 +32,7 @@ def messenger(posted_user, msg):
         "props": {
             "attachments": [
                     {
-                        "text": msg,
-                    
+                        "text": message,
                 }
             ]
         },
@@ -32,4 +44,224 @@ def messenger(posted_user, msg):
         data = json.dumps(reply_data)
     )
 
+    return reply_request
+
+# ---------------------------------------------------------------------
+# askForStdScanConfirmation
+# ---------------------------------------------------------------------
+def askForStdScanConfirmation(posted_usr,ipaddr):
+    """
+    Mattermostに対して標準スキャンの承認メッセージを送信し、ボタンのアクションに応じた処理を行うメソッド。
+
+    Parameters:
+        posted_usr (str): メッセージを投稿したユーザーのID
+        ipaddr (str): スキャン対象のIPアドレス
+
+    Returns:
+        requests.Response: Mattermostへのリクエストのレスポンス
+    """
+    reply_headers = {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer ' + BOT_TOKEN,
+    }
+
+    reply_data = {
+        "response_type": "in_channel",
+        "channel_id": CHANNEL_ID,
+        "message": f"@{posted_usr} BOCCHI reply message. :wave:",
+        "props": {
+
+            "attachments": [{
+                "title": "スキャンの実施",
+                "text": "この行為は法律に抵触する可能性があります。管理者から許可を得た端末や自身の端末に対して使用してください。\nスキャンを承認しますか？",
+                "actions": [{
+                    # Acceptボタン
+                    "name": "Accept",
+                    "integration": {
+                        "url": f"http://172.20.10.2:5000/actions/std_scan?token={BOT_TOKEN}",
+                        "context": {
+                            "text": posted_usr +","+ ipaddr
+                        }
+                    }
+                }, {
+                    # Rejectボタン
+                    "name": "Reject",
+                    "style": "danger",
+                    "integration": {
+                        "url": f"http://172.20.10.2:5000/actions/reject?token={BOT_TOKEN}"
+                    }
+                }]
+            }]
+        }
+    }
+    reply_request = requests.post(
+        MM_API_ADDRESS,
+        headers = reply_headers,
+        data = json.dumps(reply_data)
+    )
+    return reply_request
+
+# ---------------------------------------------------------------------
+# askForFullScanConfirmation
+# ---------------------------------------------------------------------
+def askForFullScanConfirmation(posted_usr,ipaddr):
+    """
+    Mattermostに対してフルポートスキャンの承認メッセージを送信し、ボタンのアクションに応じた処理を行うメソッド。
+
+    Parameters:
+        posted_usr (str): メッセージを投稿したユーザーのID
+        ipaddr (str): スキャン対象のIPアドレス
+
+    Returns:
+        requests.Response: Mattermostへのリクエストのレスポンス
+    """
+    reply_headers = {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer ' + BOT_TOKEN,
+    }
+
+    reply_data = {
+        "response_type": "in_channel",
+        "channel_id": CHANNEL_ID,
+        "message": f"@{posted_usr} BOCCHI reply message. :wave:",
+        "props": {
+
+            "attachments": [{
+                "title": "フルポートスキャンの実施",
+                "text": "この行為は法律に抵触する可能性があります。管理者から許可を得た端末や自身の端末に対して使用してください。\nフルポートスキャンを承認しますか？",
+                "actions": [{
+                    # Acceptボタン
+                    "name": "Accept",
+                    "integration": {
+                        "url": f"http://172.20.10.2:5000/actions/full_scan?token={BOT_TOKEN}",
+                        "context": {
+                            "text": posted_usr +","+ ipaddr
+                        }
+                    }
+                }, {
+                    # Rejectボタン
+                    "name": "Reject",
+                    "style": "danger",
+                    "integration": {
+                        "url": f"http://172.20.10.2:5000/actions/reject?token={BOT_TOKEN}"
+                    }
+                }]
+            }]
+        }
+    }
+    reply_request = requests.post(
+        MM_API_ADDRESS,
+        headers = reply_headers,
+        data = json.dumps(reply_data)
+    )
+    return reply_request
+
+# ---------------------------------------------------------------------
+# askForVulnScanConfirmation
+# ---------------------------------------------------------------------
+def askForVulnScanConfirmation(posted_usr,ipaddr):
+    """
+    Mattermostに対して脆弱性診断の承認メッセージを送信し、ボタンのアクションに応じた処理を行うメソッド。
+
+    Parameters:
+        posted_usr (str): メッセージを投稿したユーザーのID
+        ipaddr (str): スキャン対象のIPアドレス
+
+    Returns:
+        requests.Response: Mattermostへのリクエストのレスポンス
+    """
+    reply_headers = {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer ' + BOT_TOKEN,
+    }
+
+    reply_data = {
+        "response_type": "in_channel",
+        "channel_id": CHANNEL_ID,
+        "message": f"@{posted_usr} BOCCHI reply message. :wave:",
+        "props": {
+
+            "attachments": [{
+                "title": "脆弱性診断の実施",
+                "text": "この行為は法律に抵触する可能性があります。管理者から許可を得た端末や自身の端末に対して使用してください。\n脆弱性診断を承認しますか？",
+                "actions": [{
+                    # Acceptボタン
+                    "name": "Accept",
+                    "integration": {
+                        "url": f"http://172.20.10.2:5000/actions/vuln_scan?token={BOT_TOKEN}",
+                        "context": {
+                            "text": posted_usr +","+ ipaddr
+                        }
+                    }
+                }, {
+                    # Rejectボタン
+                    "name": "Reject",
+                    "style": "danger",
+                    "integration": {
+                        "url": f"http://172.20.10.2:5000/actions/reject?token={BOT_TOKEN}"
+                    }
+                }]
+            }]
+        }
+    }
+    reply_request = requests.post(
+        MM_API_ADDRESS,
+        headers = reply_headers,
+        data = json.dumps(reply_data)
+    )
+    return reply_request
+
+# ---------------------------------------------------------------------
+# askForBruteAttackConfirmation
+# ---------------------------------------------------------------------
+def askForBruteAttackConfirmation(posted_usr,ipaddr):
+    """
+    Mattermostに対してブルートフォース攻撃の承認メッセージを送信し、ボタンのアクションに応じた処理を行うメソッド。
+
+    Parameters:
+        posted_usr (str): メッセージを投稿したユーザーのID
+        ipaddr (str): 攻撃対象のIPアドレス
+
+    Returns:
+        requests.Response: Mattermostへのリクエストのレスポンス
+    """
+    reply_headers = {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer ' + BOT_TOKEN,
+    }
+
+    reply_data = {
+        "response_type": "in_channel",
+        "channel_id": CHANNEL_ID,
+        "message": f"@{posted_usr} BOCCHI reply message. :wave:",
+        "props": {
+
+            "attachments": [{
+                "title": "認証試行の実施",
+                "text": "この行為は法律に抵触する可能性があります。管理者から許可を得た端末や自身の端末に対して使用してください。\n認証試行を承認しますか？",
+                "actions": [{
+                    # Acceptボタン
+                    "name": "Accept",
+                    "integration": {
+                        "url": f"http://172.20.10.2:5000/actions/brute_attack?token={BOT_TOKEN}",
+                        "context": {
+                            "text": posted_usr +","+ ipaddr
+                        }
+                    }
+                }, {
+                    # Rejectボタン
+                    "name": "Reject",
+                    "style": "danger",
+                    "integration": {
+                        "url": f"http://172.20.10.2:5000/actions/reject?token={BOT_TOKEN}"
+                    }
+                }]
+            }]
+        }
+    }
+    reply_request = requests.post(
+        MM_API_ADDRESS,
+        headers = reply_headers,
+        data = json.dumps(reply_data)
+    )
     return reply_request
